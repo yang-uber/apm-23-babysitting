96a307a5f8600ae5886c7109a40bf5b5d7018d40
dterr-uber <dterr@uber.com>
2023-08-24 09:28:17 -0700

    add back button to addnewchild

--- src/Pages/AddNewChild/AddNewChild.js
+++ src/Pages/AddNewChild/AddNewChild.js
@@ -3,6 +3,7 @@ import screenshot from './addnewkid.png';  // Change the path to your actual scr
 import NextButton from '../../Components/MockScreenshot/NextButton/NextButton';
 import { useNavigate } from "react-router-dom";
 import KidInput from './kidinput';
+import { Button } from "baseui/button";
 
 function AddNewChild() {
     const navigate = useNavigate();
@@ -12,6 +13,16 @@ function AddNewChild() {
 
   return (
     <div style={{ position: 'relative', width: '375px', height: '812px', backgroundImage: `url(${screenshot})`, backgroundSize: 'cover' }}>
+        <Button onClick={() => { navigate(`/childdetails`) }} className='back-button'
+                style={{
+                    width: '50px',
+                    height: '50px',
+                    position: 'absolute',
+                    top: '5%', /* Adjust the top value as needed */
+                    left: '2%', /* Adjust the left value as needed */
+                    backgroundColor: 'blue', // Set your desired background color
+                    opacity: '0%'
+                }}></Button>
       
       <div style={{ position: 'absolute', top: '23%', left: '4%' }}>
         <KidInput  />
