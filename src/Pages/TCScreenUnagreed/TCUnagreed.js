253683f242a1c920c9e9d8915dac6b73ad5894aa
dterr-uber <dterr@uber.com>
2023-08-23 18:47:15 -0700

    Add next button on TCUnagreed

--- src/Pages/TCScreenUnagreed/TCUnagreed.js
+++ src/Pages/TCScreenUnagreed/TCUnagreed.js
@@ -4,13 +4,18 @@ import { useNavigate } from "react-router-dom";
 import { Button, SIZE } from "baseui/button";
 import MockScreenshot from '../../Components/MockScreenshot/MockScreenshot.js';
 import Image from './TCUnagreed_ss.png'; // an image is defined here using file path
+import NextButton from '../../Components/MockScreenshot/NextButton/NextButton';
 
 function TCUnagreed() {
-    let navigate = useNavigate();
+    const navigate = useNavigate();
+    const handleNextClick = () => {
+        navigate("/tcuagreed");
+    }
     return (
         <div className='Home page'>
             <MockScreenshot imageUrl={Image}> {/*each instance of MockScreenshot needs an image url passed like this */}
             </MockScreenshot>
+            <NextButton onClick={handleNextClick} />
         </div>
     );
 }
