fb9496ccda112cd680d64f31292b1c81d93920bc
yang-uber <143043676+yang-uber@users.noreply.github.com>
2023-08-24 10:37:09 -0700

    lil change

--- src/Pages/HomeScreen/HomeScreen.js
+++ src/Pages/HomeScreen/HomeScreen.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { useNavigate } from "react-router-dom";
 
 import { Button, SIZE } from "baseui/button";
-import MockScreenshot from '../../Components/MockScreenshot/MockScreenshot.js';
 import Image from './HomeScreen_ss.png'; // an image is defined here using file path
 
-function HomeScreen() {
+function HomeScreen({address}) {
     let navigate = useNavigate();
     return (
         <div className='Home page'>
@@ -46,6 +45,19 @@ function HomeScreen() {
             }}>
                 {/*make sure navigate param matches path defined in App.js*/}
             </Button>     
+            <Button onClick={() => { navigate(`/activity`) }} className='activity-bottom' // CHANGE
+            style={{
+                width: '200px',
+                height: '60px',
+                position: 'absolute',
+                top: '89%', /* Adjust the top value as needed */
+                left: '55%', /* Adjust the left value as needed */
+                backgroundColor: 'blue', // Set your desired background color
+                opacity: '100%'
+            }}>
+                {`${address}`}
+                {/*make sure navigate param matches path defined in App.js*/}
+            </Button>   
         </div>
     );
 }
